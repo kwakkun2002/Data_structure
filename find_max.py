b7cb3642cdc97235efbd6eb0953321c067cbf111
kwakkun2002 <kwakkun2002@gmail.com>
2024-03-12 15:34:13 +0900

    Create find_max.py

---
+++ find_max.py
@@ -0,0 +1,27 @@
+def find_max_recursive(l):
+    if len(l) == 1:
+        return l[-1]
+    
+    max_v = find_max_recursive(l[1:])
+
+    if l[0] > max_v:
+        return l[0]
+    else:
+        return max_v
+    
+def find_max_iterative(l):
+    max_v = 0
+    for i in l:
+        if i == l[0]:
+            max_v = i
+        else:
+            if max_v < i:
+                max_v = i
+    return max_v
+
+
+if __name__ == "__main__":
+    l = list(map(int, input("리스트 입력 : ex 1,2,3 => ").split(",")))
+    print(l)
+    print(find_max_recursive(l))
+    print(find_max_iterative(l))

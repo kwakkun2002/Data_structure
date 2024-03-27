dc6ec4333537ffc36caceafe7f84fbc67e04a5da
Gael-Android <kwakkun2002@gmail.com>
2024-03-27 12:17:28 +0900

    CustomLinkedList 구현

--- lru_cache/lru_sim.py
+++ lru_cache/lru_sim.py
@@ -1,6 +1,72 @@
+class CustomNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class CustomLinkedList:
+    def __init__(self):
+        self.tail = None
+        self.head = None
+
+    def __iter__(self):
+        curr = self.head
+        while curr is not None:
+            yield curr
+            curr = curr.next
+
+    def __contains__(self, item):
+        for curr in self:
+            if curr.value == item:
+                return True
+        return False
+
+    def append(self, value):
+        if self.head is None:  # 비어있으면 초기화
+            self.head = CustomNode(value)
+            self.tail = self.head
+        else:  # 없으면 새로 생성
+            new_node = CustomNode(value)
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def remove(self, value):
+        if self.head.value == value:
+            self.head = self.head.next
+            return
+        if self.tail.value == value:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            return
+        for curr in self:
+            if curr.value == value:
+                prev_node = curr.prev
+                next_node = curr.next
+                prev_node.next = next_node
+                next_node.prev = prev_node
+                return
+
+    def pop(self, idx):
+        curr = self.head
+        for i in range(idx):
+            curr = curr.next
+        if curr == self.head:
+            self.head = self.head.next
+        elif curr == self.tail:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            prev_node = curr.prev
+            next_node = curr.next
+            prev_node.next = next_node
+            next_node.prev = prev_node
+
+
 class LruCacheList:
     def __init__(self, capacity: int):
-        self.cache = []
+        self.cache = CustomLinkedList()
         self.size = 0
         self.capacity = capacity
 
@@ -8,24 +74,18 @@ class LruCacheList:
         if p in self.cache:  # cache hit!! => remove p and push
             self.cache.remove(p)
             self.cache.append(p)
+            # print("CACHE HIT")
             return "CACHE HIT"
         elif self.size < self.capacity:  # cache miss... => append p
             self.cache.append(p)
             self.size += 1
+            # print("CACHE MISS")
             return "CACHE MISS"
         else:  # size overflow!! => remove lru
             self.cache.pop(0)
             self.cache.append(p)
+            # print("SIZE OVERFLOW")
             return "SIZE OVERFLOW"
-
-    def view(self):
-        print(self.cache)
-
-    def get_lru_value(self):
-        return self.cache[0]
-
-    def get_mru_value(self):
-        return self.cache[-1]
 
 
 class CacheSimulator:
